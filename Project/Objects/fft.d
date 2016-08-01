9a546c56f2ddc780bb5cfd1da197d6e60ba0187b
RkTeddy <838474413@qq.com>
2016-08-01 13:21:59 +0800

    JLink test

---
+++ Project/Objects/fft.d
@@ -0,0 +1,3 @@
+.\objects\fft.o: ..\User\FFT.c
+.\objects\fft.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+.\objects\fft.o: ..\User\FFT.h
---
+++ Project/Objects/gui.d
@@ -0,0 +1,3 @@
+.\objects\gui.o: ..\User\GUI.c
+.\objects\gui.o: ..\User\picture.h
+.\objects\gui.o: ..\User\LCD_driver.h
