a32ad716ce6ddc0763850f38afa2c603e6527f7a
RkTeddy <838474413@qq.com>
2016-08-01 13:30:19 +0800

    GPIO配置

--- User/adc.c
+++ User/adc.c
@@ -10,3 +10,23 @@ void RCC_Conf()//时钟初始化
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3,ENABLE);
 }
 	
+void GPIO_Conf()//GPIO:调试时根据需要开关io防止ad口悬空
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+		
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;      //Analog--PZT
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+/*
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;     //Analog--MIC
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+	
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;      //Analog--BAT
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+	*/
+	
+}
