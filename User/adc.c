8a6ab5ef88caf9d66cba5ca418d916fa935cfe53
RkTeddy <838474413@qq.com>
2016-08-03 13:40:24 +0800

    ADC配置

--- User/adc.c
+++ User/adc.c
@@ -2,6 +2,28 @@
 
 __IO uint16_t ADCVal[2048];//adc数据储存
 
+void adc_start(u8 mode) //mode ：PZT压电 MIC麦克
+{
+	switch(mode)
+	{
+		case PZT:
+		{
+			ADC_Cmd(ADC1,DISABLE);
+			ADC_RegularChannelConfig(ADC1,ADC_Channel_15,1,ADC_SampleTime_239Cycles5);
+			ADC_Cmd(ADC1,ENABLE);
+			break;
+		}
+		/*case MIC:
+		{
+			ADC_Cmd(ADC1,DISABLE);
+			ADC_RegularChannelConfig(ADC1,ADC_Channel_9,1,ADC_SampleTime_239Cycles5);
+			ADC_Cmd(ADC1,ENABLE);
+			break;
+		}*/
+	}		
+	TIM_Cmd(TIM3,ENABLE);	
+}
+
 void RCC_Conf()//时钟初始化
 {
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1,ENABLE);
@@ -48,7 +70,7 @@ void NVIC_Conf()   //tim中断，组1优先级0副优先级1
 	NVIC_InitTypeDef NVIC_InitStructure;
 	
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
-		
+	
 	NVIC_InitStructure.NVIC_IRQChannel=DMA1_Channel1_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=0;
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority=1;
@@ -78,4 +100,34 @@ void DMA_Conf()
 	DMA_ClearITPendingBit(DMA1_IT_TC1);
 	DMA_ITConfig(DMA1_Channel1,DMA_IT_TC,ENABLE);
 	DMA_Cmd(DMA1_Channel1,ENABLE);
+}
+
+void ADC1_Conf()//规则组：pzt&mic  注入组：电池
+{
+	ADC_InitTypeDef ADC_InitStructure;
+
+	ADC_InitStructure.ADC_Mode=ADC_Mode_Independent;
+	ADC_InitStructure.ADC_ScanConvMode=ENABLE;
+	ADC_InitStructure.ADC_ContinuousConvMode=DISABLE;
+	ADC_InitStructure.ADC_ExternalTrigConv=ADC_ExternalTrigConv_T3_TRGO;
+	ADC_InitStructure.ADC_DataAlign=ADC_DataAlign_Right;
+	ADC_InitStructure.ADC_NbrOfChannel=1;
+	ADC_Init(ADC1,&ADC_InitStructure);
+	RCC_ADCCLKConfig(RCC_PCLK2_Div8);
+	//ADC_RegularChannelConfig(ADC1,ADC_Channel_0,1,ADC_SampleTime_239Cycles5);
+	
+	/*
+	ADC_TempSensorVrefintCmd(ENABLE);
+	ADC_InjectedSequencerLengthConfig(ADC1, 2);
+	ADC_ExternalTrigInjectedConvConfig(ADC1, ADC_ExternalTrigInjecConv_None);
+  ADC_InjectedChannelConfig(ADC1,ADC_Channel_Vrefint, 1, ADC_SampleTime_55Cycles5);
+	ADC_InjectedChannelConfig(ADC1,ADC_Channel_2, 2, ADC_SampleTime_55Cycles5);*/
+	ADC_DMACmd(ADC1,ENABLE);
+	ADC_ExternalTrigConvCmd(ADC1,ENABLE);
+	//ADC_ExternalTrigInjectedConvCmd(ADC1, DISABLE);   
+	ADC_Cmd(ADC1,ENABLE);
+	ADC_ResetCalibration(ADC1);
+	while(ADC_GetResetCalibrationStatus(ADC1));
+	ADC_StartCalibration(ADC1);
+	while(ADC_GetCalibrationStatus(ADC1));
 }
