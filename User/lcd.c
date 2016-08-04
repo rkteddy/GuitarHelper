37fd89b759bcf3544949a024dbc880b75d9a6771
RkTeddy <838474413@qq.com>
2016-08-04 19:50:40 +0800

    数据发送

--- User/lcd.c
+++ User/lcd.c
@@ -11,3 +11,24 @@ void LCD_Writ_Bus(char VH,char VL)    // 并行数据写入函数
 		LCD_WR(0);
 		LCD_WR(1); 
 }
+
+void LCD_Write_COM(char VH,char VL)  	 // 发送命令
+{	
+    LCD_RS(0);
+		LCD_Writ_Bus(VH,VL);
+}
+
+void LCD_Write_DATA(char VH,char VL)  	// 发送数据
+{
+    LCD_RS(1);
+		LCD_Writ_Bus(VH,VL);
+}
+
+ void LCD_Write_COLOR(char V1,char V2,char V3)	  // 发送数据
+{
+		LCD_RS(1);  
+		LCD_DB17((V1 >> 5) & 0x01);   
+		LCD_DB16((V1 >> 4) & 0X01);
+		LCD_Writ_Bus(((V2 >> 2) & 0x0f)+((V1 << 4)& 0xf0),((V2 << 6)& 0xc0) + (V3 & 0x3f)); 	  // rgb 666 转 288	
+}	   
+
