80184cdbd7b78f1718ae4b5fadf7f3d71f69530b
RkTeddy <838474413@qq.com>
2016-08-11 20:38:54 +0800

    界面切换

--- User/GUI.c
+++ User/GUI.c
@@ -265,6 +265,105 @@ void GUI()
 						flag=0;	
 				}
 		}
+		if (change)    // 判断画面有没有发生改变	
+		switch (page)    // 根据page的变化更新页面显示内容
+		{
+				case 0:
+						Piant(color[1]);    // 涂黑page0 选模式
+						Rechangle(4,4,232,154,color[5]);
+						Rechangle(4,162,232,154,color[5]);
+						Drawsingle(60,4,120,120,1800,color[0],color[5],tune);
+						Drawsingle(60,162,120,120,1800,color[0],color[5],chord);
+						LCD_ShowString(24,122,color[0],color[5],"Guitar Tuner");
+						LCD_ShowString(8,280,color[0],color[5],"Chord Analysis");
+						change=0;
+						break;
+				case 1:
+						Piant(color[1]);    // 涂黑page1 选乐器
+					  Rechangle(4,2,232,83,color[5]);
+						Drawsingle(30,2,120,80,1200,color[0],color[5],guitar);
+						LCD_ShowString(120,52,color[0],color[5],"Guitar");
+						Rechangle(4,87,232,83,color[5]);
+						Drawsingle(30,87,120,80,1200,color[0],color[5],bass);
+						LCD_ShowString(136,137,color[0],color[5],"Bass");					
+						Rechangle(4,172,232,83,color[5]);
+						Drawsingle(20,172,120,80,1200,color[0],color[5],ukulele);
+						LCD_ShowString(112,222,color[0],color[5],"Ukulele");
+						Rechangle(4,257,114,62,color[5]);
+						Drawsingle(35,270,48,36,216,color[0],color[5],arrowleft);
+						Rechangle(122,257,114,62,color[5]);
+						Drawsingle(162,280,32,16,64,color[0],color[5],battery);
+						change=0;
+						break;
+				case 2:
+						Piant(color[1]);    // 涂黑page2 选弦
+					  Rechangle(4,4,114,80,color[5]);
+					  LCD_ShowString(5,46,color[0],color[5],"String1");
+					  Rechangle(122,4,114,80,color[5]);
+					  LCD_ShowString(123,46,color[0],color[5],"String2");
+					  Rechangle(4,88,114,80,color[5]);
+					  LCD_ShowString(5,130,color[0],color[5],"String3");
+					  Rechangle(122,88,114,80,color[5]);
+					  LCD_ShowString(123,130,color[0],color[5],"String4");
+					  Rechangle(4,172,114,80,color[5]);
+					  LCD_ShowString(5,214,color[0],color[5],"String5");
+					  Rechangle(122,172,114,80,color[5]);
+					  LCD_ShowString(123,214,color[0],color[5],"String6");
+					 
+					  Rechangle(4,257,114,62,color[5]);
+					  Drawsingle(35,270,48,36,216,color[0],color[5],arrowleft);
+					  Rechangle(122,257,114,62,color[5]);
+					  Drawsingle(162,280,32,16,64,color[0],color[5],battery);
+					  change=0;
+					  break;
+        case 3:
+						Piant(color[1]);    // 涂黑page3 选音
+				    Rechangle(3,4,76,59,color[5]);
+						LCD_ShowString(33,22,color[0],color[5],"A");
+						Rechangle(82,4,76,59,color[5]);
+						LCD_ShowString(104,22,color[0],color[5],"#A");
+					  Rechangle(161,4,76,59,color[5]);
+					  LCD_ShowString(191,22,color[0],color[5],"B");
+					 
+					  Rechangle(3,67,76,59,color[5]);
+					  LCD_ShowString(33,85,color[0],color[5],"C");
+					  Rechangle(82,67,76,59,color[5]);
+					  LCD_ShowString(104,85,color[0],color[5],"#C");
+					  Rechangle(161,67,76,59,color[5]);
+					  LCD_ShowString(191,85,color[0],color[5],"D");
+					 
+					  Rechangle(3,130,76,59,color[5]);
+					  LCD_ShowString(25,148,color[0],color[5],"#D");
+					  Rechangle(82,130,76,59,color[5]);
+					  LCD_ShowString(112,148,color[0],color[5],"E");
+					  Rechangle(161,130,76,59,color[5]);
+					  LCD_ShowString(191,148,color[0],color[5],"F");
+					 
+					  Rechangle(3,193,76,59,color[5]);
+					  LCD_ShowString(25,211,color[0],color[5],"#F");
+					  Rechangle(82,193,76,59,color[5]);
+					  LCD_ShowString(112,211,color[0],color[5],"G");
+					  Rechangle(161,193,76,59,color[5]);
+					  LCD_ShowString(183,211,color[0],color[5],"#G");
+					 
+					  Rechangle(4,257,114,62,color[5]);
+					  Drawsingle(35,270,48,36,216,color[0],color[5],arrowleft);
+					  Rechangle(122,257,114,62,color[5]);
+					  Drawsingle(162,280,32,16,64,color[0],color[5],battery);
+					  change=0;
+					  break;
+				case 4:
+						Piant(color[1]);    // 涂黑
+					  Rechangle(4,4,232,248,color[5]);
+					  Drawsingle(60,45,120,120,1800,color[0],color[5],tuning);
+					  LCD_ShowString(48,180,color[0],color[5],"Tuning...");
+					  Rechangle(4,257,114,62,color[5]);
+					  Drawsingle(35,270,48,36,216,color[0],color[5],arrowleft);
+					  Rechangle(122,257,114,62,color[5]);
+					  Drawsingle(162,280,32,16,64,color[0],color[5],battery);
+					  change=0;
+					  break;	
+				}
 		 
 		LCD_CS (1);    // 关闭片选使能	
    }
