a3904fac2f58ee0c6b6b1161cb70e34a20844252
RkTeddy <838474413@qq.com>
2016-08-14 20:58:41 +0800

    SPI write data

--- User/touch.c
+++ User/touch.c
@@ -18,3 +18,18 @@ void SPI_Start()                                     //SPI开始
 		CS(0);
 		Delay(150); 
 }
+//**********************************************************
+void WriteCharTo7843(unsigned char num)          //SPI写数据
+{
+		unsigned char count=0;
+		DCLK(0);
+		for(count=0;count<8;count++)
+		{
+				if((num<<count)&0x80)
+						DIN(1);   /*判断发送位*/
+				else
+						DIN(0);   
+				DCLK(0); Delay(150);                //上升沿有效
+				DCLK(1); Delay(150);
+		}
+}
