37701baf5c796b3c817b495636c5082ad6291e2d
RkTeddy <838474413@qq.com>
2016-08-14 20:59:31 +0800

    SPI read data

--- User/touch.c
+++ User/touch.c
@@ -23,7 +23,7 @@ void WriteCharTo7843(unsigned char num)          //SPI写数据
 {
 		unsigned char count=0;
 		DCLK(0);
-		for(count=0;count<8;count++)
+		for (count=0;count<8;count++)
 		{
 				if((num<<count)&0x80)
 						DIN(1);   /*判断发送位*/
@@ -32,4 +32,26 @@ void WriteCharTo7843(unsigned char num)          //SPI写数据
 				DCLK(0); Delay(150);                //上升沿有效
 				DCLK(1); Delay(150);
 		}
+}
+//**********************************************************
+unsigned int ReadFromCharFrom7843()             //SPI 读数据
+{
+	unsigned char count=0;
+	unsigned int Num=0;
+	
+	for (count=0;count<12;count++)
+	{
+			Num<<=1;
+			DCLK(1);
+			Delay(150);                //下降沿有效
+			DCLK(0);
+			Delay(150);
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7 ; // 选择所有脚
+			GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;	 //推挽输出
+			GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	 //输出的最大频率为50HZ
+			GPIO_Init(GPIOB, &GPIO_InitStructure); 
+			if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_7))
+					Num++;
+	}
+	return (Num);
 }
