fceaee513211b38f8aa7903f4d65ea802fbd4f4f
RkTeddy <838474413@qq.com>
2016-08-14 20:58:01 +0800

    SPI initialize

---
+++ User/touch.c
@@ -0,0 +1,20 @@
+
+#include "stm32f10x.h"
+#include "touch.h"
+
+extern GPIO_InitTypeDef GPIO_InitStructure;
+
+//**********************************************************
+void SPI_Start()                                     //SPI¿ªÊ¼
+{
+		DCLK(0);
+		Delay(100);
+		CS(1);
+		Delay(150); 
+		DIN(1);
+		Delay(100);
+		DCLK(1);
+		Delay(200);
+		CS(0);
+		Delay(150); 
+}
