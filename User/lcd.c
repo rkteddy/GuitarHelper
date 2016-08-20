1fe7eaea47daa070a3c6ca4ffc01212d35c55532
RkTeddy <838474413@qq.com>
2016-08-20 22:04:04 +0800

    LCD噪点解决

--- User/lcd.c
+++ User/lcd.c
@@ -10,29 +10,35 @@ void LCD_Init(void)
 		delayms(5);
 		LCD_RST(1);
 		delayms(5);
-	
-		LCD_CS(0);    // 打开片选使能
+
+		// 打开片选使能	
+		LCD_CS(0);
 		                                                                                                                                                                                                                                                       
     delayms(10);
 	
-    LCD_Write_COM(0x00,0x83);           
-		LCD_Write_DATA(0x00,0x02);    // TESTM=1 
+		// TESTM=1
+		LCD_Write_COM(0x00,0x83);           
+		LCD_Write_DATA(0x00,0x02); 
 				 
+    // VDC_SEL=011
 		LCD_Write_COM(0x00,0x85);  
-		LCD_Write_DATA(0x00,0x03);    // VDC_SEL=011
+		LCD_Write_DATA(0x00,0x03);
 		LCD_Write_COM(0x00,0x8B);  
-		LCD_Write_DATA(0x00,0x01); 
-		LCD_Write_COM(0x00,0x8C);  
-		LCD_Write_DATA(0x00,0x93);    // STBA[7]=1, STBA[5:4]=01, STBA[1:0]=11
+		LCD_Write_DATA(0x00,0x01);
+
+    // STBA[7]=1, STBA[5:4]=01, STBA[1:0]=11		
+		LCD_Write_COM(0x00,0x8C); 
+		LCD_Write_DATA(0x00,0x93);
 		
-		LCD_Write_COM(0x00,0x91);  
-		LCD_Write_DATA(0x00,0x01);    // DCDC_SYNC=1
-		
+    // DCDC_SYNC=1
+		LCD_Write_COM(0x00,0x91); 
+		LCD_Write_DATA(0x00,0x01);
+
+    // TESTM=0
 		LCD_Write_COM(0x00,0x83);  
-		LCD_Write_DATA(0x00,0x00);    // TESTM=0
+		LCD_Write_DATA(0x00,0x00);
 		
 		// Gamma Setting
-
 		LCD_Write_COM(0x00,0x3E);  
 		LCD_Write_DATA(0x00,0xB0);
 		LCD_Write_COM(0x00,0x3F);  
@@ -58,7 +64,7 @@ void LCD_Init(void)
 		LCD_Write_COM(0x00,0x49);  
 		LCD_Write_DATA(0x00,0x40);	
 		
-//**********Power On sequence************
+		// Power On sequence
 		
 		LCD_Write_COM(0x00,0x17);  
 		LCD_Write_DATA(0x00,0x91); 
@@ -74,7 +80,7 @@ void LCD_Init(void)
 		LCD_Write_DATA(0x00,0x11); 
 					
 		LCD_Write_COM(0x00,0x1C);  
-		LCD_Write_DATA(0x00,0x06);  // 0d
+		LCD_Write_DATA(0x00,0x06);
 		
 		LCD_Write_COM(0x00,0x1F);  
 		LCD_Write_DATA(0x00,0x42);
@@ -96,7 +102,7 @@ void LCD_Init(void)
 		LCD_Write_DATA(0x00,0x27);
 		delayms(100);	   
 		
-//**********DISPLAY ON SETTING***********
+		// Display on setting
         
 		LCD_Write_COM(0x00,0x24);  
 		LCD_Write_DATA(0x00,0x60);	
@@ -150,47 +156,67 @@ void LCD_Init(void)
 		LCD_CS(1); 
 }  
 
-void delayms(int count)  // /* X1ms */
+void delayms(int count)
 {
 		int i,j;
 		for(i=0;i<count;i++)
 			for(j=0;j<5000;j++);
 }
 
-void LCD_Writ_Bus(char VH,char VL)    // 并行数据写入函数
+/*
+ 并行数据写入函数
+*/
+void LCD_Writ_Bus(char VH,char VL)
 {
-    GPIOA->BSRR = VH & 0x00ff;  	 // 高位P1口
+  	 // 高位P1口
+    GPIOA->BSRR = VH & 0x00ff;
     GPIOA->BRR  = (~VH) & 0x00ff;
-    GPIOB->BSRR = (VL<<8) & 0xff00;   	// 低位P0口
+	
+   	// 低位P0口
+    GPIOB->BSRR = (VL<<8) & 0xff00;
     GPIOB->BRR  = (~(VL)<<8) & 0xff00;
 		LCD_WR(0);
 		LCD_WR(1); 
 }
 
-void LCD_Write_COM(char VH,char VL)  	 // 发送命令
+/*
+ 发送命令
+*/
+void LCD_Write_COM(char VH,char VL)
 {	
     LCD_RS(0);
 		LCD_Writ_Bus(VH,VL);
 }
 
-void LCD_Write_DATA(char VH,char VL)  	// 发送数据
+/*
+ 发送数据
+*/
+void LCD_Write_DATA(char VH,char VL)
 {
     LCD_RS(1);
 		LCD_Writ_Bus(VH,VL);
 }
 
- void LCD_Write_COLOR(char V1,char V2,char V3)	  // 发送数据
+/*
+ 发送数据
+*/
+ void LCD_Write_COLOR(char V1,char V2,char V3)
 {
 		LCD_RS(1);  
 		LCD_DB17((V1 >> 5) & 0x01);   
 		LCD_DB16((V1 >> 4) & 0X01);
-		LCD_Writ_Bus(((V2 >> 2) & 0x0f)+((V1 << 4)& 0xf0),((V2 << 6)& 0xc0) + (V3 & 0x3f)); 	  // rgb 666 转 288	
+	
+ 	  // rgb 666 转 288	
+		LCD_Writ_Bus(((V2 >> 2) & 0x0f)+((V1 << 4)& 0xf0),((V2 << 6)& 0xc0) + (V3 & 0x3f));
 }	   
 
 void Piant(const char *c)
 {
 		int i,j;
-		LCD_CS(0);    // 打开片选使能
+	
+    // 打开片选使能
+		LCD_CS(0);
+	
 		Address_set(0,0,239,319);
 		for (i=0;i<320;i++)
 		{
@@ -199,13 +225,21 @@ void Piant(const char *c)
 						LCD_Write_COLOR(*c,*(c+1),*(c+2));
 				}
 		}
-		LCD_CS(1);    // 关闭片选使能
+		
+    // 关闭片选使能
+		LCD_CS(1);
 }
 
-void Rechangle(int x1,int y1,int x,int y,const char *c)    // 指定区域画矩形
+/*
+ 指定区域画矩形
+*/
+void Rechangle(int x1,int y1,int x,int y,const char *c)
 {
 		int i,j;
-		LCD_CS(0);    // 打开片选使能
+	
+    // 打开片选使能
+		LCD_CS(0);
+	
 		Address_set(x1,y1,x1+x-1,y1+y-1);
     for(i=0;i<y;i++)
 		{
@@ -214,19 +248,28 @@ void Rechangle(int x1,int y1,int x,int y,const char *c)    // 指定区域画矩形
 						LCD_Write_COLOR(*c,*(c+1),*(c+2));
 				}
 	  }
-    LCD_CS(1);    // 关闭片选使能
+		
+    // 关闭片选使能
+    LCD_CS(1);
 }
 
-void Draw(uint x1,uint y1,uint x,uint y,uint m,const unsigned char*p)    // 指定区域作彩色图
+/*
+ 指定区域作彩色图
+*/
+void Draw(uint x1,uint y1,uint x,uint y,uint m,const unsigned char*p)
 {   
 		int n;
-		unsigned char k=0;    // 横坐标计数变量
-	  int l=0;    // 纵坐标计数变量
-		char i=1;    // 是否需要重新写地址
+    // 横坐标计数变量
+		unsigned char k=0;
+    // 纵坐标计数变量
+	  int l=0;
+    // 是否需要重新写地址
+		char i=1;
 	  LCD_CS(0);
 		for (n=0;n<m/3;n++)	   
-	  {	
-				if (*p==0x3f&&*(p+1)==0x00&&*(p+2)==0x3f){i=1;}    // 紫色则跳过
+	  {
+				// 紫色则跳过
+				if (*p==0x3f&&*(p+1)==0x00&&*(p+2)==0x3f){i=1;}
 			  else
 			  { 
 						if(i)
@@ -235,62 +278,81 @@ void Draw(uint x1,uint y1,uint x,uint y,uint m,const unsigned char*p)    // 指定
 								i=0;
 						} 
 				    LCD_Write_COLOR(*p,*(p+1),*(p+2));
-				} 
+				}
+				
+		    // 横坐标增加
 				p=p+3;
-				k++;    // 横坐标增加
+				k++;
+				
+				// x个像素后，k重新归零
 				if(k==x)
 				{
 					 i=1;
 					 k=0;
 					 l++; 
-				}    // x个像素后，k重新归零		 
+				}		 
 	  }
     LCD_CS(1);		
 }
 
-void Drawsingle(uint x1,uint y1,uint x,uint y,uint m,const char *f,const char *b,const unsigned char*p)    // 指定区域作画,单色
-{   int n;
+/*
+ 指定区域作画,单色
+*/
+void Drawsingle(uint x1,uint y1,uint x,uint y,uint m,const char *f,const char *b,const unsigned char*p)
+{   
+		int n;
 	  char k=0;
 	  char i;
-	unsigned char j;
+		unsigned char j;
 	  LCD_CS(0);
-	   Address_set(x1,y1,x1+x-1,y1+y-1) ;    // 设置地址范围
-			 for (n=0;n<m;n++)	   
-	     {	
-				  j=*p;
+	
+	   // 设置地址范围
+	  Address_set(x1,y1,x1+x-1,y1+y-1);
+		for (n=0;n<m;n++)
+		{	
+				j=*p;
 				for(k=0;k<8;k++) 
 				{
-					
-					i=j&0x80;    // 取出字节最高位
-					j=j<<1;
-					i=i>>7;
-				if(i)
-           {
-					 LCD_Write_COLOR(*f,*(f+1),*(f+2));
-					 }
-			  else
-			     { 
-				     
-				     LCD_Write_COLOR(*b,*(b+1),*(b+2));
-					 } 
-				 }    
-		    p++;     // 数组指针自加
-	     }
+				    // 取出字节最高位
+						i=j&0x80;
+						j=j<<1;
+						i=i>>7;
+						if(i)
+						{
+								LCD_Write_COLOR(*f,*(f+1),*(f+2));
+						}
+						else
+						{ 
+								LCD_Write_COLOR(*b,*(b+1),*(b+2));
+						} 
+				}
+				
+    // 数组指针自加
+		p++;
+		}
+		
     LCD_CS(1);		
 }
 
+/*
+ 设置地址范围
+*/
 void Address_set(uint x1,uint y1,uint x2,uint y2)
 {  
-		LCD_Write_COM(0x00,0x02);LCD_Write_DATA(x1,x1>>8);    // 开始X
+    // 开始X
+		LCD_Write_COM(0x00,0x02);LCD_Write_DATA(x1,x1>>8);
 		LCD_Write_COM(0x00,0x03);LCD_Write_DATA(x1>>8,x1);
 
-		LCD_Write_COM(0x00,0x06);LCD_Write_DATA(y1,y1>>8);    // 开始Y
+    // 开始Y
+		LCD_Write_COM(0x00,0x06);LCD_Write_DATA(y1,y1>>8);
 		LCD_Write_COM(0x00,0x07);LCD_Write_DATA(y1>>8,y1);
 
-		LCD_Write_COM(0x00,0x04);LCD_Write_DATA(x2,x2>>8);    // 结束X
+    // 结束X
+		LCD_Write_COM(0x00,0x04);LCD_Write_DATA(x2,x2>>8);
 		LCD_Write_COM(0x00,0x05);LCD_Write_DATA(x2>>8,x2);
 
-		LCD_Write_COM(0x00,0x08);LCD_Write_DATA(y2,y2>>8);    // 结束Y
+    // 结束Y
+		LCD_Write_COM(0x00,0x08);LCD_Write_DATA(y2,y2>>8);
 		LCD_Write_COM(0x00,0x09);LCD_Write_DATA(y2>>8,y2);
 
 		LCD_Write_COM(0x00,0x22);							 
@@ -301,21 +363,28 @@ void LCD_ShowChar(uint x,uint y,const char *f,const char *b,char ch)
 		unsigned char temp , Bytes;
 		unsigned char pos,t;
 		unsigned int  CHAR_W,CHAR_H;
-
-		 
-		// CHAR_W = 8;         // 8*16
+	  
+		// 8*16
+		// CHAR_W = 8;
 		// CHAR_H = 16;
 
-		CHAR_W = 16;         // 16*24
+    // 16*24
+		CHAR_W = 16;
 		CHAR_H = 24;
-		LCD_CS(0);    // 片选打开
+
+    // 片选打开
+		LCD_CS(0);
 		if(x>(LCD_SIZE_X-CHAR_W)||y>(LCD_SIZE_Y-CHAR_H))
 				return;
 		 
 		Address_set(x, y, x+CHAR_W-1, y+CHAR_H-1);
-		ch = ch-32;    // 按照ASCII编码顺序的到相应字母的编码
+		
+    // 按照ASCII编码顺序的到相应字母的编码
+		ch = ch-32;
 		Bytes = (CHAR_W*CHAR_H)/8;
-		for(pos=0;pos<Bytes;pos++)    // CHAR_H 	(CHAR_W*CHAR_H)/8
+		
+    // CHAR_H 	(CHAR_W*CHAR_H)/8
+		for(pos=0;pos<Bytes;pos++)
 		{
 				//temp= Font8x16[ch][pos];
 				//temp= Font16x24[ch][pos];
@@ -328,8 +397,10 @@ void LCD_ShowChar(uint x,uint y,const char *f,const char *b,char ch)
 								LCD_Write_COLOR(*b,*(b+1),*(b+2));
 						temp<<=1; 
 				}
-		 }
-		 LCD_CS(1);//关闭片选
+		}
+		
+		// 关闭片选
+		LCD_CS(1);
 }  
 
 void LCD_ShowString(uint x,uint y,const char *f,const char *b,char *p)
