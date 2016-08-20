6e787e4e6eeb1aac4bb47553b52a0d295a9018ba
RkTeddy <838474413@qq.com>
2016-08-20 22:04:59 +0800

    ADC头

--- User/adc.h
+++ User/adc.h
@@ -3,8 +3,6 @@
 主程序初始化调用void adc_init(void)
 开始转化调用void adc_start(u8 mode)  参数：PZT 压电陶瓷 MIC麦克风
 在dma中断DMA1_Channel1_IRQHandler()里调用fft
-
-
 */
 
 #ifndef __ADC_H
@@ -17,9 +15,6 @@
 #define PZT 0X01
 #define MIC 0X02
 
-
-
-
 void adc_init(void);
 void RCC_Conf(void);
 void TIM3_Conf(void);
@@ -28,6 +23,5 @@ void NVIC_Conf(void);
 void ADC1_Conf(void);
 void DMA_Conf(void);
 void adc_start(u8 mode);
-
 
 #endif
