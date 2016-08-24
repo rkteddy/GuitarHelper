94eb2a65621f1fd27997fbb8c6d0396c73e59d15
RkTeddy <838474413@qq.com>
2016-08-24 22:27:32 +0800

    外部中断配置

--- User/main.c
+++ User/main.c
@@ -198,3 +198,14 @@ void RCC_Configuration(void)
 				while(RCC_GetSYSCLKSource() != 0x08);
 		}
 }
+
+void NVIC_Configuration(void)
+{
+		#ifdef  VECT_TAB_RAM  
+			/* Set the Vector Table base location at 0x20000000 */ 
+			NVIC_SetVectorTable(NVIC_VectTab_RAM, 0x0); 
+		#else  /* VECT_TAB_FLASH  */
+			/* Set the Vector Table base location at 0x08000000 */ 
+			NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0x0);   
+		#endif
+}
