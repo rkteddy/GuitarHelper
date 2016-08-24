0c23b40b6ec6d1fdaf43484dcbb45dc194ac9531
RkTeddy <838474413@qq.com>
2016-08-24 22:26:21 +0800

    LCD及图形界面相关GPIO配置

--- User/main.c
+++ User/main.c
@@ -54,3 +54,102 @@ int main(void)
 		}
 }
 
+
+void GUI_Init()
+{
+			// 系统时钟配置函数
+			RCC_Configuration(); 
+     // NVIC配置函数  
+			NVIC_Configuration();
+			// 启动GPIO模块时钟
+			RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC | RCC_APB2Periph_AFIO, ENABLE);
+			// 把调试设置普通IO口
+			// GPIO_PinRemapConfig(GPIO_Remap_SWJ_Disable,ENABLE);  
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
+			// 所有GPIO为同一类型端口
+			GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+			// 推挽输出
+			GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+			// 输出的最大频率为50HZ
+			GPIO_Init(GPIOB, &GPIO_InitStructure);
+			// 初始化GPIOB端口
+		
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;	 
+			GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+			GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+			// 初始化GPIOA端口
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;	 
+			GPIO_Init(GPIOA, &GPIO_InitStructure);  
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;	 
+			GPIO_Init(GPIOA, &GPIO_InitStructure);
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;	 
+			GPIO_Init(GPIOA, &GPIO_InitStructure);
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;	 
+			GPIO_Init(GPIOA, &GPIO_InitStructure);
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;	 
+			GPIO_Init(GPIOA, &GPIO_InitStructure);
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;	 
+			GPIO_Init(GPIOA, &GPIO_InitStructure);
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;	 
+			GPIO_Init(GPIOA, &GPIO_InitStructure);
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;	 
+			GPIO_Init(GPIOA, &GPIO_InitStructure);
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;	 
+			GPIO_Init(GPIOA, &GPIO_InitStructure);
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;	 
+			GPIO_Init(GPIOA, &GPIO_InitStructure);
+		
+			GPIO_InitStructure.GPIO_Mode=GPIO_Mode_AIN;
+
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;	 
+			GPIO_Init(GPIOA, &GPIO_InitStructure);
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;	 
+			GPIO_Init(GPIOA, &GPIO_InitStructure);
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;	 
+			GPIO_Init(GPIOA, &GPIO_InitStructure);		
+			// 选择PB3
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 ;
+			// 开漏输入
+			GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+			// 输出的最大频率为50HZ
+			GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+			// 初始化GPIOB端口
+			GPIO_Init(GPIOB, &GPIO_InitStructure); 
+
+			// 将GPIOA 16个端口全部置为高电平
+		  GPIO_Write(GPIOA,0xffff);
+		  // 将GPIOB 16个端口全部置为高电平
+		  GPIO_Write(GPIOB,0xffff);
+
+		  LCD_Init();
+		  delayms(150); 
+		
+		  AD7843(CMD_RDX);
+		  AD7843(CMD_RDY);
+		  
+			// 涂黑
+		  Piant(color[1]);
+			// 第一幅图，电池电量检测
+		  LCD_ShowString(16,107,color[0],color[1],"Guitar Helper");
+		  LCD_ShowString(64,133,color[0],color[1],"Battery");
+		  LCD_ShowString(32,161,color[0],color[1],"Checking");
+			// 图标
+		  Drawsingle(96,200,48,48,288,color[2],color[1],test);
+		  LCD_ShowString(160,161,color[0],color[1],".");
+		  delayms(500);
+		  LCD_ShowString(176,161,color[0],color[1],".");
+		  delayms(500);
+		  LCD_ShowString(192,161,color[0],color[1],".");
+		  delayms(500);
+		  Rechangle(160,161,208,184,color[1]);
+		 
+		 // 涂黑后显示page0
+		  Piant(color[1]);
+		  Rechangle(4,4,232,154,color[5]);
+		  Rechangle(4,162,232,154,color[5]);
+		  Drawsingle(60,4,120,120,1800,color[0],color[5],tune);
+		  Drawsingle(60,162,120,120,1800,color[0],color[5],chord);
+		  LCD_ShowString(24,122,color[0],color[5],"Guitar Tuner");
+		  LCD_ShowString(8,280,color[0],color[5],"Chord Analysis");
+}
