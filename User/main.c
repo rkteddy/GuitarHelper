4dfbaa4641fa782d01fd1a3ee9d35e254471ec1a
RkTeddy <838474413@qq.com>
2016-08-24 22:26:56 +0800

    时钟配置

--- User/main.c
+++ User/main.c
@@ -153,3 +153,48 @@ void GUI_Init()
 		  LCD_ShowString(24,122,color[0],color[5],"Guitar Tuner");
 		  LCD_ShowString(8,280,color[0],color[5],"Chord Analysis");
 }
+
+void RCC_Configuration(void)
+{   
+	  // 复位RCC外部设备寄存器到默认值
+		RCC_DeInit();
+
+		// 打开外部高速晶振
+		RCC_HSEConfig(RCC_HSE_ON);
+
+		// 等待外部高速时钟准备好
+		HSEStartUpStatus = RCC_WaitForHSEStartUp();
+
+		if (HSEStartUpStatus == SUCCESS)
+		{								    
+				// 开启FLASH的预取功能
+				FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
+
+				// FLASH延迟2个周期
+				FLASH_SetLatency(FLASH_Latency_2);
+			
+				// 配置AHB(HCLK)时钟=SYSCLK
+				RCC_HCLKConfig(RCC_SYSCLK_Div1);  
+			
+				// 配置APB2(PCLK2)钟=AHB时钟
+				RCC_PCLK2Config(RCC_HCLK_Div1); 
+
+				// 配置APB1(PCLK1)钟=AHB 1/2时钟
+				RCC_PCLK1Config(RCC_HCLK_Div2);
+
+				 // 配置PLL时钟 == 外部高速晶体时钟*9  PLLCLK = 8MHz * 9 = 72 MHz 
+				RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9);
+
+				// 使能PLL时钟
+				RCC_PLLCmd(ENABLE);
+
+			 // 等待PLL时钟就绪
+				while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);
+				
+				// 配置系统时钟 = PLL时钟
+				RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+
+				// 检查PLL时钟是否作为系统时钟
+				while(RCC_GetSYSCLKSource() != 0x08);
+		}
+}
