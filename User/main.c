63063ffde8df3de7c29fee2e64a4fd70ad72e061
RkTeddy <838474413@qq.com>
2016-08-25 22:29:11 +0800

    串口配置

--- User/main.c
+++ User/main.c
@@ -225,3 +225,31 @@ void DMA1_Channel1_IRQHandler()   // DMAÖÐ¶Ï
 				FFT(s);		
 		}
 }		
+
+void USART_Conf()
+{
+		USART_InitTypeDef USART_InitStructure;
+		GPIO_InitTypeDef GPIO_InitStructure;
+		
+		
+		
+		GPIO_InitStructure.GPIO_Pin=GPIO_Pin_9;//Tx
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+		GPIO_Init(GPIOA,&GPIO_InitStructure);
+		
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;//Rx
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+		GPIO_Init(GPIOA,&GPIO_InitStructure);
+		
+		USART_InitStructure.USART_BaudRate=115200;
+		USART_InitStructure.USART_WordLength=USART_WordLength_8b;
+		USART_InitStructure.USART_StopBits=USART_Parity_No;
+		USART_InitStructure.USART_HardwareFlowControl=USART_HardwareFlowControl_None;
+		USART_InitStructure.USART_Mode=USART_Mode_Rx|USART_Mode_Tx;
+		USART_Init(USART1,&USART_InitStructure);
+		USART_ITConfig(USART1,USART_IT_RXNE,ENABLE);
+		USART_Cmd(USART1,ENABLE);
+		USART_ClearFlag(USART1,USART_FLAG_TC);
+		USART_ClearITPendingBit(USART1,USART_IT_RXNE);
+}
